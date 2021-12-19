6c00f54c9a0e7a90b0888fcdb9c4c41f4cfc2f98
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2021-12-19 15:38:45 +0600

    send email using sendgrid

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -124,4 +124,11 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 # CRISPY_TEMPLATE_PACK = 'bootstrap4'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'munna.cse.pust@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = 'sendgrid_password'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
